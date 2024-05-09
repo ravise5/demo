23d6f82095d3b6293d4ceea4b3091313831fa715
Ravisanker E <ravisanker1999@gmail.com>
2024-05-09 14:26:24 +0530

    Update functions.js

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -72,7 +72,7 @@ function formatEmailInput(field) {
  * @return {string}
  */
 function setErrorMessage(globals) {
-  globals.functions.markFieldAsInvalid('$form.field', 'This field has errror, please check!');
+  globals.functions.markFieldAsInvalid('$form.field', 'This field has errror, please check!', { useDataRef: true});
 }
 
 
